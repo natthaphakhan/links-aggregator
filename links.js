13f4c4d31d97a4dc063ea34161d40685daed743c
natthaphakhan <73375595+natthaphakhan@users.noreply.github.com>
2025-10-18 19:12:17 +0700

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,7 @@
 const links = [
   { title: 'Life Weeks Visualizer', url: 'https://natthaphakhan.github.io/life_weeks_visualizer/' },
   { title: 'Virtual Pet Game', url: 'https://natthaphakhan.github.io/virtual-pet-game/' },
-  { title: 'Breath Box', url: 'https://natthaphakhan.github.io/breath-box/' }
+  { title: 'Breath Box', url: 'https://natthaphakhan.github.io/breath-box/' },
+  { title: 'Breathing 4-7-8', url: 'https://natthaphakhan.github.io/4-7-8-breathing/' }
+  
 ];
